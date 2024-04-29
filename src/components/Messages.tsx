7a8e3c2ef2ce01dd138f8f689963b2c2ebafb2d5
DavidxDl <davidbekamlopez@gmail.com>
2024-04-29 18:23:47 -0400

    -remove unnecesary div

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -20,15 +20,13 @@ export default function Messages({ userId, destineId, shouldRefresh }: Props) {
     getMessages().catch(err => console.error(err));
   }, [shouldRefresh])
   return (
-    <div className='pb-2 rounded-t bg-white w-96 h-96 border-b '>
-      <ul className='gap-1 overflow-y-scroll text-black flex flex-col justify-end h-full'>
-        {messages?.map(m => (
-          <li key={m.id} className={`bg-gradient-to-tr from-red-600 to-sky-600 text-white rounded p-1 flex justify-end ${m.authorId === userId && 'self-end'}`}>
-            {m.message}
-          </li>))
-        }
-      </ul>
-    </div>
+    <ul className=' pb-2 rounded-t bg-white w-96 h-96 border-b gap-1 overflow-y-scroll text-black flex flex-col justify-end '>
+      {messages?.map(m => (
+        <li key={m.id} className={`bg-gradient-to-tr from-red-600 to-sky-600 text-white rounded p-1 flex justify-end ${m.authorId === userId && 'self-end'}`}>
+          {m.message}
+        </li>))
+      }
+    </ul>
   )
 }
 
