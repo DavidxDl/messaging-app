4e91f610ca2716de7ccf2637e7d86d2d69385e07
DavidxDl <davidbekamlopez@gmail.com>
2024-04-29 18:22:10 -0400

    -added now the username of the friend you chatting with appears as the header

--- src/app/friends/[friendId]/page.tsx
+++ src/app/friends/[friendId]/page.tsx
@@ -1,14 +1,22 @@
 
+import { RedirectToSignIn } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs/server';
+import { User } from '@prisma/client';
 import Chat from '~/components/Chat';
 import MessageInput from '~/components/MessageInput';
+import { db } from '~/server/db';
 
 
 export default async function page({ params }: { params: { friendId: string } }) {
   const { userId } = auth()
+  if (!userId)
+    <RedirectToSignIn />
+
+  const friend: User | null = await db.user.findUnique({ where: { id: params.friendId } })
 
   return (
     <main>
+      {!!friend && <h1 className='text-2xl font-extrabold'>{friend.username}</h1>}
       <Chat userId={userId!} destineId={params.friendId} />
     </main>
   )
