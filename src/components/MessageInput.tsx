16a6c4b4a73f05419e9d8900748142f6c5ae4c30
DavidxDl <davidbekamlopez@gmail.com>
2024-04-29 18:06:54 -0400

    -added some style to the chat
    in other words
    
    style has entered the chat

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -24,12 +24,12 @@ export default function MessageInput({ destineId, authorId, refresh }: Props) {
   }
 
   return (
-    <form method='POST' onSubmit={async (e) => {
+    <form className='flex' method='POST' onSubmit={async (e) => {
       e.preventDefault();
       await onSubmit(message)
     }}>
-      <input value={message} onChange={(e) => setMessage(m => e.target.value)} className="flex-grow  outline-none text-black py-2 px-4" type='text' name='message' placeholder='send something!' />
-      <input type='submit' value="Send" />
+      <input value={message} onChange={(e) => setMessage(m => e.target.value)} className="flex-grow   outline-none text-black py-2 px-4" type='text' name='message' placeholder='send something!' />
+      <input className='w-16 border border-black rounded hover:scale-x-110 z-10 bg-blue-900 ' type='submit' value="Send" />
     </form>
   )
 }
