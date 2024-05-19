2ede077cea717ae0b9ac0b0f8f71114154c60fc4
DavidxDl <davidbekamlopez@gmail.com>
2024-05-19 02:05:38 -0400

    fix overflow on the friendList Component

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -20,7 +20,7 @@ export default function FriendList({ friends }: Props) {
   return (
     <div className="self-start flex flex-col bg-black/60 rounded-xl p-2 h-full  md:min-w-full overflow-hidden flex-nowrap">
       <h1 className="text-xl self-start my-3 ">Friends</h1>
-      <ul>{friends?.map(f =>
+      <ul className="overflow-scroll">{friends?.map(f =>
         <li key={f.friends.id}>
           <Link className="flex gap-2 items-center hover:font-extrabold text-white" href={`/friends/${f.friends.id}`}>
             <Avatar imageUrl={f.friends.imgUrl} />
